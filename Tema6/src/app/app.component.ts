db7cb7c8897a89f6cfdb55b9349a6f15e269e57d
Cruceru Lucas <lucascruceru@gmail.com>
2019-01-23 20:35:31 +0200

    base for tema 6

---
+++ Tema6/src/app/app.component.ts
@@ -0,0 +1,42 @@
+import {Component, Input} from '@angular/core';
+import { Platform } from 'ionic-angular';
+import { StatusBar } from '@ionic-native/status-bar';
+import { SplashScreen } from '@ionic-native/splash-screen';
+import { HomePage } from '../pages/home/home';
+@Component({
+  templateUrl: 'app.html'
+})
+export class MyApp {
+  rootPage:any = HomePage;
+  likes:number = 0;
+  comments: Comment[] = [];
+
+  constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen) {
+    platform.ready().then(() => {
+      // Okay, so the platform is ready and our plugins are available.
+      // Here you can do any higher level native things you might need.
+      statusBar.styleDefault();
+      splashScreen.hide();
+    });
+  }
+  postComment(){
+    this.comments.push(new Comment("Cruceru Lucas", "Lorem Ipsum"));
+  }
+
+  incrementLikes(){
+    this.likes = this.likes + 1;
+  }
+
+}
+export class Comment {
+  username:string;
+  text:string;
+
+  constructor(username:string, text:string){
+    this.username = username;
+    this.text = text;
+  }
+
+}
+
+
